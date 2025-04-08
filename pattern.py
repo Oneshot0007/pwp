edd113f1cd9415798234c883f9d70fb659d5f5ff
Prasad Chauke <146482320+Oneshot0007@users.noreply.github.com>
2025-04-08 19:43:48 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,10 @@
+# i =1
+# while i <= 5:
+#    print(i * " * ")
+#    i = i + 1
+
+# # name = input("Enter The Nmae -->")
+# # print("Length of your string is ",len(name))
+
+# str = " Doller$"
+# print(str.count("$"))

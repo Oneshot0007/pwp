c086303b29ac2874bea1cee88e298f369debee97
Prasad Chauke <146482320+Oneshot0007@users.noreply.github.com>
2025-04-08 20:58:38 +0530

    # 16. Write a Python Program to display symmetric difference of two sets.

---
+++ Q16.py
@@ -0,0 +1,4 @@
+# 16. Write a Python Program to display symmetric difference of two sets.
+s1={1,2,3,4,5}
+s2={5,6,7,8,9}
+print("Set Symmentric Difference of 2 set is ",s1.symmetric_difference(s2))

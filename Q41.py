dfcf9b1d1a61ccc6b01ba85a7052362b4b5f1b6c
Prasad Chauke <146482320+Oneshot0007@users.noreply.github.com>
2025-04-09 20:56:26 +0530

    41. Write a Python program to perform following operations on set: intersection of sets,  union of sets, set difference, symmetric difference, clear a set.

---
+++ Q41.py
@@ -0,0 +1,10 @@
+set1={1,2,4,5}
+set2={4,5,6,7}
+print(set1.union(set2))
+print(set1.intersection(set2))
+print(set1.difference(set2))
+print(set1.symmetric_difference(set2))
+#clearing the set
+set1.clear()
+set2.clear()
+print(set1,set2)

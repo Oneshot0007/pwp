84a4dbf122796b3c445d49f3fde71492264fa262
Prasad Chauke <146482320+Oneshot0007@users.noreply.github.com>
2025-04-09 15:26:30 +0530

    26. Write a Python program to multiply all the items in a list

---
+++ Q26.py
@@ -0,0 +1,9 @@
+# 26. Write a Python program to multiply all the items in a list
+
+my_list = [1, 2, 3, 4, 5]
+
+result = 1
+for item in my_list:
+    result *= item
+
+print("Product of all items in the list:", result)
